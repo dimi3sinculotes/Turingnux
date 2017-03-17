e708fbe159d9ca82ce99f3f588e907217538245f
dimi3sinculotes <rufus111296@gmail.com>
2017-03-17 10:04:53 +0100

    Init Project

---
+++ src/practica2si/Practica2SI.java
@@ -0,0 +1,17 @@
+package practica2si;
+
+import java.util.Scanner;
+
+
+public class Practica2SI {
+
+    
+    public static void main(String[] args) {
+        Scanner scaner = new Scanner(System.in);
+        System.out.println("VVVVVVVVVVVVVVVVVVVVVVVVVVVVVVV");
+        System.out.println("VVVV WELLCOME TO TURINGNUX VVVV");
+        System.out.println("VVVVVVVVVVVVVVVVVVVVVVVVVVVVVVV");
+        System.out.println("\n" + "Select mode:");
+    }
+    
+}
