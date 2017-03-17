bc5a31ade7d2c55fae25526799e0a8fe4efb341d
dimi3sinculotes <rufus111296@gmail.com>
2017-03-17 11:56:32 +0100

    Cesar wip

--- src/practica2si/Practica2SI.java
+++ src/practica2si/Practica2SI.java
@@ -1,5 +1,7 @@
 package practica2si;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Scanner;
 
 
@@ -8,10 +10,55 @@ public class Practica2SI {
     
     public static void main(String[] args) {
         Scanner scaner = new Scanner(System.in);
+        String mode = "";
+        String input = "";
+        
+        Map<Character, Integer> map = new HashMap<Character, Integer>();        
+        map = initMap(map);
+        
         System.out.println("VVVVVVVVVVVVVVVVVVVVVVVVVVVVVVV");
         System.out.println("VVVV WELLCOME TO TURINGNUX VVVV");
         System.out.println("VVVVVVVVVVVVVVVVVVVVVVVVVVVVVVV");
         System.out.println("\n" + "Select mode:");
+        System.out.println("\nA: Cesar");
+        System.out.println("B: Vigenere");
+        System.out.println("C: Transposition");
+        while((!mode.equals("a")) && (!mode.equals("A")) && (!mode.equals("b")) && 
+                (!mode.equals("B")) && (!mode.equals("c")) && (!mode.equals("C"))){
+            
+            System.out.println("\nSelect valid mode:");
+            mode = scaner.nextLine();
+        }
+        System.out.println("\nmode " + mode + " selected");
+        switch(mode){
+            case "a":
+            case "A":
+                System.out.println("\nSelect the key for Cesar alg");
+                int key = Integer.parseInt(scaner.nextLine());
+                System.out.println("Type or Paste the coded text to try to decode");
+                input = scaner.nextLine();
+                cesarAlg(key, input, map);
+                break;
+                
+            case "b":
+            case "B":
+                break;
+                
+            case "c":
+            case "C":
+                break;
+                
+            default:
+                System.out.println("MODE ERROR");
+                break;
+        }
     }
-    
+    public static Map<Character, Integer> initMap(Map<Character, Integer> map){
+         
+        
+        return map;
+    }
+    public static void cesarAlg(int key, String input,Map<Character, Integer> map){
+        
+    }
 }
